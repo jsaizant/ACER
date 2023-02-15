d2e7d36c3d91e755d77777b50476f6f7e6b8e6da
Javier Saiz <74248167+jsaizant@users.noreply.github.com>
2023-02-15 17:55:12 +0000

    Completed get sql questions function

--- AskMySQL/src/model/cleanData.py
+++ AskMySQL/src/model/cleanData.py
@@ -24,6 +24,9 @@ SQL_STATEMENTS = [
     "DECLARE"
     ]
 
+# Set tags to filter posts
+TAGS = "sql-server|mysql|postgresql|oracle|t-sql|mariadb"
+
 ### FUNCTIONS
 
 def parse_XML(xml_file): 
@@ -68,8 +71,7 @@ def extract_code_snippets(string):
     """
 
     pattern = re.compile(r'<code>(.*?)</code>', re.DOTALL)
-    sql_queries = [code for code in pattern.findall(string) 
-                    if is_sql_query(code)]
+    sql_queries = [code for code in pattern.findall(string) if is_sql_query(code)]
     if sql_queries != []:
       return sql_queries[0]
     else:
@@ -80,14 +82,73 @@ def get_sql_questions_answers(file_path):
     Read Posts.xml from Stackexchange/ Stackoverflow data dump and load
     questions and answers containing SQL queries into a dataframe.
     """
-    ### TODO complete the function
+    # Read file
     posts_df = parse_XML(file_path)
+    
+    # Get relevant columns
     posts_df = posts_df[['Id', 'PostTypeId', 'CreationDate', 'Body', 'Title', 'Tags', 'ContentLicense', 'AcceptedAnswerId']]
+    
+    # Set corresponding questions and answers at the same level
     questanswr_df = pd.merge(
+        # Subset of questions
         left=posts_df[(posts_df["PostTypeId"] == "1") & (posts_df["AcceptedAnswerId"].notna())][["Title", "Tags", "AcceptedAnswerId", 'CreationDate', 'ContentLicense']],
+        # Subset of answers
         right=posts_df[(posts_df["PostTypeId"] == "2")][["Id", "Body"]], 
         left_on="AcceptedAnswerId", 
         right_on="Id", 
         how="inner"
         )
+
+    print(f"Rows without filtering: {questanswr_df.shape}")
+    
+    # Filter questions with tags
+    questanswr_df = questanswr_df[(questanswr_df['Tags'].str.contains(TAGS))]
+    print(f"Rows after filtering tags: {questanswr_df.shape}")
+
+    # Filter answers with code snippets
+    questanswr_df = questanswr_df[(questanswr_df['Body'].str.contains("<code>"))]
+    print(f"Rows after filtering answers with code: {questanswr_df.shape}")
+
+    # Extract the first SQL query from the answers
+    questanswr_df["SQLQuery"] = questanswr_df["Body"].map(lambda x: extract_code_snippets(x))
+
+    # Drop any row with no queries
+    questanswr_df = questanswr_df.dropna()
+    print(f"Rows after filtering answers with SQL queries: {questanswr_df.shape}")
+
+    # Select relevant columns
+    questanswr_df = questanswr_df[["Id", "Title", "Tags", "CreationDate", "ContentLicense", "SQLQuery"]]
+
+    return questanswr_df
+
+def compile_dataframe_to_jsonlines(df):
+    # Create new list
+    json_lines = []
+
+    # Iterate over dataframe
+    for _, row in df.iterrows():
+
+        # Set metadata dictionary
+        metadata = {
+            "id": row["Id"],
+            "creationdate": row["CreationDate"],
+            "license": row["ContentLicense"],
+            "tags": row["Tags"]
+        }
+
+        # Set JSON line
+        json_line = {
+            "text": row["Title"],
+            "code": row["SQLQuery"],
+            "metadata": metadata
+        }
+
+        # Append to main list
+        json_lines.append(json.dumps(json_line))
+    return json_lines
+
+def save_jsonlines(path, json_lines):
+    # Write the JSON lines to a file
+    with open(path, "w") as f:
+        f.write("\n".join(json_lines))
 
